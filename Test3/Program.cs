ec02b1431bb5c8d44287efafa7ca3387b12c9199
nehy06001 <nehy06001@E-PW05CGQV>
2024-09-12 13:49:25 +0200

    Add project files.

---
+++ Test3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift_4
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Detta ser lite ut som den svenska flaggan");
+        }
+    }
+}
